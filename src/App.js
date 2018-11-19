71f04985cd323a5e6bf023781785e59d8c694fbd
lucas <cerfectus@gmail.com>
2018-11-19 12:56:22 -0600

    react-cardio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+constructor(){
+  super()
+  this.state = {a: ""};
+
+
+}
+update(){
+  this.setState({
+    a: this.a.value,
+    b: this.b.value
+  })
+}
+render(){
+return(
+  <div>
+    
+    <input ref={node => this.a  = node} type="text" onChange={this.update.bind(this)}/>
+    
+    {this.state.a};
+    <hr />
+    <input ref={ node => this.b = node} type="text" onChange={this.update.bind(this)}/>
+    {this.state.b};
+
+  </div>
+)
 }
 
+
+}
+  
 export default App;
