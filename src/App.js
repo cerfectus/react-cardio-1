599a356e157927d31aab8af8510ec406b417f7cb
lucas <cerfectus@gmail.com>
2018-11-19 13:37:11 -0600

    cardio

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
-constructor(){
-  super()
-  this.state = {a: ""};
+  constructor() {
+    super();
+    this.state = { val: 0 };
+    this.update = this.update.bind(this);
+  }
+  //metodo update
+  update() {
+    this.setState({ val: this.state.val + 1 });
+  }
+  //antes del render
+  componentWillMount() {
+    console.log("componentWillMount");
+    this.setState(({m: 2}))
+  }
 
-
+  render() {
+    console.log("render");
+    return <button onClick={this.update}>
+    {this.state.val * this.state.m}</button>;
+  }
+  //despues del render
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.inc=setInterval(this.update, 500)
+  }
+  //acabar el render
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    clearInterval(this.inc);
+  }
 }
-update(){
-  this.setState({
-    a: this.a.value,
-    b: this.b.value
-  })
-}
-render(){
-return(
-  <div>
-    
-    <input ref={node => this.a  = node} type="text" onChange={this.update.bind(this)}/>
-    
-    {this.state.a};
-    <hr />
-    <input ref={ node => this.b = node} type="text" onChange={this.update.bind(this)}/>
-    {this.state.b};
+//extiende App y le da quien sabe que
+class Wrapper extends React.Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById("a"));
+  }
+  unMount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById("a"));
+  }
+  render(){
+    return(
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unMount.bind(this)}>UnMount</button>
+        <div id= "a"></div>
 
-  </div>
-)
+      </div>
+    )
+  }
 }
-
-
-}
-  
-export default App;
+export default Wrapper;
